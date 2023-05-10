eeb60e4067e676d7603173cf894e59ba7f979efb
Darryn Flood <Darryn.Flood@optiisolutions.com>
2023-05-10 19:42:28 +1000

    Added comments

--- OrleansBlogPosts.Api/Grains/BlogPostGrain.cs
+++ OrleansBlogPosts.Api/Grains/BlogPostGrain.cs
@@ -10,6 +10,9 @@ namespace OrleansBlogPosts.Api.Grains
         public Task<BlogPost> GetBlogPost();
     }
 
+    /// <summary>
+    /// Blog post gain
+    /// </summary>
     public class BlogPostGrain : Grain, IBlogPostGrain
     {
         private readonly IPersistentState<BlogPost> _state;
@@ -19,15 +22,23 @@ namespace OrleansBlogPosts.Api.Grains
             _state = state;
         }
 
+        /// <summary>
+        /// Create a new blog post
+        /// </summary>
         public async Task CreateBlogPostAsync(BlogPost newBlogPost)
         {
+            // Save the blog post to state
             _state.State = newBlogPost;
             await _state.WriteStateAsync();
 
+            // Add the blog post to the index (index grain id is default 0)
             var blogPosts = GrainFactory.GetGrain<IBlogPostsIndexGrain>(0);
             await blogPosts.AddToIndex(newBlogPost);
         }
 
+        /// <summary>
+        /// Retrieve the blog post
+        /// </summary>
         public Task<BlogPost> GetBlogPost()
         {
             var blogPost = _state.State;
--- OrleansBlogPosts.Api/Grains/BlogPostsIndexGrain.cs
+++ OrleansBlogPosts.Api/Grains/BlogPostsIndexGrain.cs
@@ -10,6 +10,9 @@ namespace OrleansBlogPosts.Api.Grains
         public Task AddToIndex(BlogPost blogPost);
     }
 
+    /// <summary>
+    /// Blog Post index grain
+    /// </summary>
     public class BlogPostsIndexGrain : Grain, IBlogPostsIndexGrain
     {
         private readonly IPersistentState<HashSet<long>> _blogPostIds;
@@ -20,11 +23,16 @@ namespace OrleansBlogPosts.Api.Grains
             _blogPostIds = state;
         }
 
+        /// <summary>
+        /// When grain is activated, populated the list of Blog Posts
+        /// </summary>
         public override async Task OnActivateAsync(CancellationToken cancellationToken)
         {
+            // Don't do anything if we don't have any ids stored
             if (_blogPostIds is not { State.Count: > 0 })
                 return;
 
+            // Populate a list of blog posts by looping through each stored id and retrieving the blog post grain
             await Parallel.ForEachAsync(
                 _blogPostIds.State,
                 async (id, cancellationToken) =>
@@ -34,16 +42,25 @@ namespace OrleansBlogPosts.Api.Grains
                 });
         }
 
+        /// <summary>
+        /// Add a blog post id to the index.
+        /// </summary>
         public Task AddToIndex(BlogPost blogPost)
         {
+            // Add the blog post id
             _blogPostIds.State ??= new();
             _blogPostIds.State.Add(blogPost.Id);
 
+            // Add the blog post to the list
             _blogPosts.Add(blogPost);
 
+            // Save
             return _blogPostIds.WriteStateAsync();
         }
 
+        /// <summary>
+        /// Return a list of Blog Posts that can be used to populate a grid that can be filtered, sorted and paged
+        /// </summary>
         public Task<HashSet<BlogPost>> GetBlogPosts()
         {
             return Task.FromResult(_blogPosts);
